172cfcdf2af2b28aeab874d5f4892e620b10e2da
Yvonne Yu <monkey6277@gmail.com>
2014-04-20 23:50:26 +0800

    update

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 
 $(document).ready(function() {
     $(window).scroll(function () {
-        if ($(window).scrollTop() >= 320) {
+        if ($(window).scrollTop() >= 330) {
             $('#navSection').addClass('navbar-fixed');
         }
-        if ($(window).scrollTop() < 320) {
+        if ($(window).scrollTop() < 330) {
             $('#navSection').removeClass('navbar-fixed');
         }
     });
